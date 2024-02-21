93b1e6afc6da6b24857be310ef6c81a103ac79b3
Untitled26 <82178586+Untitled26@users.noreply.github.com>
2024-02-21 12:28:28 -0500

    Add files via upload

---
+++ Capsule.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capsule : MonoBehaviour
+{
+    int var2 = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float var1 = 1.5f;
+        var2 = 11;
+
+        Debug.Log(var1 + " " + var2);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Cube.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    string var4 = "Gaming";
+        string var5 = "is";
+    string var6 = "Life";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(var4 + var5 + var6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
